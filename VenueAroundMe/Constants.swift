284736b0afb0d1bd7beec0c6c1784eb41edd0b90
Triet Le <letrongtriet@gmail.com>
2021-04-29 16:36:39 +0300

    Venue Around Me

--- VenueAroundMe/Constants.swift
+++ VenueAroundMe/Constants.swift
@@ -2,8 +2,8 @@ import Foundation
 
 enum Constants {
     enum API {
-        static let apiKey = "CYEMKOM4OLTP5PHMOFVUJJAMWT5CH5G1JBCYREATW21XLLSZ"
-        static let apiSecret = "GYNP4URASNYRNRGXR5UEN2TGTKJHXY5FGSAXTIHXEUG1GYM2"
+        static let apiKey = ""
+        static let apiSecret = ""
         static let apiVersion = "20180323"
         static let limit = "20"
     }
